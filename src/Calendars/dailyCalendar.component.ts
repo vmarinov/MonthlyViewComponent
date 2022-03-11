cfc2a2e67bd242c94de115b75b4778a7e23da04f
Ventsislav Marinov <vmarinovrs@gmail.com>
2022-03-11 15:28:54 +0200

    Daily View first commit

---
+++ src/Calendars/dailyCalendar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'daily-calendar',
+    templateUrl: 'daily_calendar.template.html',
+    styleUrls: ['dailyCalendar.css']
+})
+export class DailyCalendarComponent {
+
+}
