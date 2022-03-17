2e43d41dd630a49d25940051f9dcae5af2abf794
Ventsislav Marinov <ventsislav.marinov@infinno.eu>
2022-03-17 11:25:25 +0200

    Changes to left nav calendar

--- src/Calendars/dailyCalendar.component.ts
+++ src/Calendars/dailyCalendar.component.ts
@@ -3,6 +3,9 @@ import { Subscription } from "rxjs";
 import { CalendarEventsService } from "src/Services/calendarEventsService.class";
 
 const HOURS = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const HRS = ['1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM',
+    '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM',
+    '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM'];
 
 @Component({
     selector: 'daily-calendar',
@@ -18,7 +21,6 @@ export class DailyCalendarComponent implements OnDestroy {
     eventsSubscription: Subscription;
     dateSubscription: Subscription;
     hours = HOURS;
-    hourlyEvents: Map<any, any> = new Map<any, any>();
     hoursAndEvents: Map<any, any> = new Map<any, any>();
     events: any;
 
@@ -54,7 +56,7 @@ export class DailyCalendarComponent implements OnDestroy {
             this.events.forEach((element: any) => {
                 Object.assign(element,
                     {
-                        interval: Math.abs(element.ends.substring(0, 2) - element.starts.substring(0, 2)) + 1,
+                        interval: Math.abs(element.ends.substring(0, 2) - element.starts.substring(0, 2)),
                         startTime: element.starts.substring(0, 2)
                     })
             });
