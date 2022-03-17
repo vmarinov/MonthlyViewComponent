c42c6d71fa7b32f2578017a609d83e81254a8ea0
Ventsislav Marinov <ventsislav.marinov@infinno.eu>
2022-03-17 17:15:42 +0200

    * Made changes to how we store start and end time of an event * Added selectedEvents and getEvents() to service * Changes to setEvents() in daily view * Changes to template of daily view and styles

--- src/Calendars/dailyCalendar.component.ts
+++ src/Calendars/dailyCalendar.component.ts
@@ -1,11 +1,9 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnDestroy } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnDestroy, OnInit } from "@angular/core";
 import { Subscription } from "rxjs";
 import { CalendarEventsService } from "src/Services/calendarEventsService.class";
 
-const HOURS = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-const HRS = ['1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM',
-    '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM',
-    '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM'];
+const HOURS = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const HRS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
 
 @Component({
     selector: 'daily-calendar',
@@ -13,32 +11,38 @@ const HRS = ['1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM',
     styleUrls: ['dailyCalendar.css'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DailyCalendarComponent implements OnDestroy {
+export class DailyCalendarComponent implements OnInit, OnDestroy {
     currentDate = new Date();
     selectedDate!: number;
     shownDate: any;
     dayOfWeek!: string;
     eventsSubscription: Subscription;
     dateSubscription: Subscription;
-    hours = HOURS;
+    hours = HRS;
     hoursAndEvents: Map<any, any> = new Map<any, any>();
     events: any;
 
-
     constructor(@Inject('CalendarEventsService') private calendarEventsService: CalendarEventsService,
-        @Inject('weekDays') private weekDays: any,
-        private changeRef: ChangeDetectorRef) {
+        @Inject('weekDays') private weekDays: any, private changeRef: ChangeDetectorRef) {
 
         this.eventsSubscription = this.calendarEventsService.eventsShown$.subscribe((events: any) => {
-            this.events = events;
-            this.setEvents();
+            this.clearEvents();
+            this.setEvents(events);
         });
         this.dateSubscription = this.calendarEventsService.dateShown$.subscribe((date: any) => {
             this.changeRef.markForCheck();
-            this.shownDate = date;
-            this.selectedDate = date.getDate();
-            this.dayOfWeek = this.weekDays[date.getDay()];
+            this.setDate(date);
+            this.clearEvents();
+            this.setEvents(this.calendarEventsService.getEvents());
         });
+    }
+
+    ngOnInit(): void {
+        for (let hr of this.hours) {
+            let hour = hr % 12 == 0 ? '12' : `${hr % 12}`;
+            hour += hr < 11 ? ' AM' : ' PM'
+            this.hoursAndEvents.set(hr, { hour, events: [] });
+        }
     }
 
     ngOnDestroy(): void {
@@ -50,17 +54,49 @@ export class DailyCalendarComponent implements OnDestroy {
         return hr;
     }
 
-    setEvents() {
-        if (this.events) {
-            this.changeRef.markForCheck();
-            this.events.forEach((element: any) => {
-                Object.assign(element,
-                    {
-                        interval: Math.abs(element.ends.substring(0, 2) - element.starts.substring(0, 2)),
-                        startTime: element.starts.substring(0, 2)
-                    })
-            });
+    setDate(date: any) {
+        this.shownDate = date;
+        this.selectedDate = date.getDate();
+        this.dayOfWeek = this.weekDays[date.getDay()];
+    }
+
+    setEvents(events: any) {
+        this.events = events;
+        this.events = this.events.filter((event: any) => {
+            return event.date.getDate() == this.shownDate.getDate() &&
+                (event.date.getMonth() == this.shownDate.getMonth() ||
+                    event.date.getMonth() == this.shownDate.getMonth() - 1 ||
+                    event.date.getMonth() == this.shownDate.getMonth() + 1) &&
+                event.date.getFullYear() == this.shownDate.getFullYear()
+        });
+
+        this.events.forEach((element: any) => {
+            let startTime = element.starts.substring(0, 2);
+            let endTime = element.ends.substring(0, 2);
+            let duration = `${startTime}${startTime < 11 && endTime > 11 ? 'am' : ''} - ${endTime}${endTime < 11 ? 'am' : 'pm'}`;
+            let interval = endTime - startTime;
+            Object.assign(element,
+                {
+                    interval,
+                    startTime,
+                    duration
+                })
+        });
+
+        if (this.events.length > 0) {
+            for (let event of this.events) {
+                let hourlyEventObj = this.hoursAndEvents.get(Number(event.startTime));
+                hourlyEventObj.events.push(event);
+            }
         }
+        this.changeRef.detectChanges();
+    }
+
+    clearEvents() {
+        for (let hour of this.hoursAndEvents.values()) {
+            hour.events = [];
+        }
+        this.changeRef.detectChanges();
     }
 
     //add fn to calculate width of events element
--- src/Services/calendarEventsService.class.ts
+++ src/Services/calendarEventsService.class.ts
@@ -5,24 +5,30 @@ import { BehaviorSubject, Subject } from 'rxjs';
 export class CalendarEventsService {
   events = [
     {
-      name: 'Available', starts: '12:00 PM', ends: '13:00 PM', calendar: 'emilia', date: new Date(2022, 2, 22),
+      name: 'Available', starts: '12:00', ends: '13:00', calendar: 'emilia', date: new Date(2022, 2, 22),
     },
     {
-      name: 'Marathon', starts: '16:00 PM', ends: '20:00 PM', calendar: 'emilia', date: new Date(2022, 2, 22),
+      name: 'Marathon', starts: '16:00', ends: '20:00', calendar: 'emilia', date: new Date(2022, 2, 22),
     },
     {
-      name: 'Meet', starts: '10:00 AM', ends: '12:00 PM', calendar: 'emilia', date: new Date(2022, 2, 10),
+      name: 'Meet', starts: '10:00', ends: '12:00', calendar: 'emilia', date: new Date(2022, 2, 10),
     }
     ,
     {
-      name: 'Jog', starts: '10:00 AM', ends: '12:00 PM', calendar: 'john', date: new Date(2022, 2, 12),
+      name: 'Jog', starts: '10:00', ends: '12:00', calendar: 'john', date: new Date(2022, 2, 12),
     },
     {
-      name: 'Jog', starts: '10:00 AM', ends: '12:00 PM', calendar: 'john', date: new Date(2022, 3, 1),
+      name: 'Jog', starts: '10:00', ends: '12:00', calendar: 'john', date: new Date(2022, 3, 1),
     },
     ,
     {
-      name: 'Available', starts: '10:00 AM', ends: '12:00 PM', calendar: 'emilia', date: new Date(2022, 3, 2),
+      name: 'Available', starts: '10:00', ends: '12:00', calendar: 'emilia', date: new Date(2022, 3, 2),
+    },
+    {
+      name: 'Jog', starts: '10:00', ends: '13:00', calendar: 'emilia', date: new Date(2022, 3, 2),
+    },
+    {
+      name: 'Meet', starts: '11:00', ends: '13:00', calendar: 'emilia', date: new Date(2022, 3, 2),
     }];
 
   eventsSource = new BehaviorSubject<any>(null);
@@ -32,10 +38,15 @@ export class CalendarEventsService {
   eventsShown$ = this.eventsSource.asObservable();
   dateShown$ = this.dateSource.asObservable();
   leftNavDateShown$ = this.leftNavDateSource.asObservable();
+  eventsSelected: any;
 
   setEvents(events: any) {
-    events = this.events.filter((event: any) => events.has(event.calendar));
-    this.eventsSource.next(events);
+    this.eventsSelected = this.events.filter((event: any) => events.has(event.calendar));
+    this.eventsSource.next(this.eventsSelected);
+  }
+
+  getEvents() {
+    return this.eventsSelected;
   }
 
   setDate(date: any) {
